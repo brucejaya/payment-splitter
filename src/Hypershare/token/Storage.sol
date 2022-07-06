8ba52c34916f8eaa601e8ffbedd0a83f5366a2a2
brucejayasinghe <brucejay43@gmail.com>
2022-07-06 14:39:46 +0100

    first commit

---
+++ src/Hypershare/token/Storage.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import '../../Interface/ICompliance.sol';
+import '../../Interface/IIdentityRegistry.sol';
+
+contract TokenStorage {
+    /// @dev ERC20 basic variables
+    mapping(address => uint256) internal _balances;
+    mapping(address => mapping(address => uint256)) internal _allowances;
+    uint256 internal _totalSupply;
+
+    /// @dev Token information
+    string internal tokenName;
+    string internal tokenSymbol;
+    uint8 internal tokenDecimals;
+    address internal tokenOnchainID;
+    string internal constant TOKEN_VERSION = '3.5.1';
+
+    /// @dev Variables of freeze and pause functions
+    mapping(address => bool) internal frozen;
+    mapping(address => uint256) internal frozenTokens;
+
+    bool internal tokenPaused = false;
+
+    /// @dev Identity Registry contract used by the onchain validator system
+    IIdentityRegistry internal tokenIdentityRegistry;
+
+    /// @dev Compliance contract linked to the onchain validator system
+    ICompliance internal tokenCompliance;
+}
