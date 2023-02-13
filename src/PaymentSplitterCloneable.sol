6fbe975bc7728055052b5079b2c7e8c36119994a
brucejayasinghe <brucejay43@gmail.com>
2023-02-13 10:03:23 +0000

    Reformatted

--- src/PaymentSplitterCloneable.sol
+++ src/PaymentSplitterCloneable.sol
@@ -2,161 +2,23 @@
 
 pragma solidity >=0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/utils/Context.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "openzeppelin-contracts/contracts/utils/Context.sol";
+import "openzeppelin-contracts/contracts/finance/PaymentSplitter.sol";
 
-contract PaymentSplitterCloneable is Context {
+contract PaymentSplitterCloneable is PaymentSplitter, Context {
     
-    using SafeERC20 for IERC20;
+    constructor() parent(address[], uint256[]) {}
 
-    event PayeeAdded(address account, uint256 shares);
-    event PaymentReleased(address to, uint256 amount);
-    event PaymentReceived(address from, uint256 amount);
-    
-    uint256 private _totalShares;
-    uint256 private _totalTokenReleased;
-    uint256 private _totalEthReleased;
-
-    mapping(address => uint256) private _shares;
-    mapping(address => uint256) private _ethReleased;
-    mapping(address => uint256) private _tokenReleased;
-
-    address public paymentToken;
-    address[] private _payees;
-
-    constructor() {}
-
-    function initialize(address[] memory payees, uint256[] memory shares_, address _paymentToken) public {
-        require(_payees.length == 0, "PaymentSplitterCloneable: already initialized");
+    function initialize(
+		address[] memory payees,
+		uint256[] memory shares_
+	) payable {
         require(payees.length == shares_.length, "PaymentSplitter: payees and shares length mismatch");
         require(payees.length > 0, "PaymentSplitter: no payees");
-        
+
         for (uint256 i = 0; i < payees.length; i++) {
             _addPayee(payees[i], shares_[i]);
         }
-        paymentToken = _paymentToken;
     }
 
-    /**
-     * @dev For handling network tokens i.e. matic, eth
-     */
-    receive() external payable virtual {
-        emit PaymentReceived(_msgSender(), msg.value);
-    }
-   
-    /**
-     * @dev Returns total shares
-     */
-    function totalShares() public view returns (uint256) { 
-        return _totalShares;
-    }
-
-    /**
-     * @dev Returns the amount of shares by address
-     */
-    function shares(address account) public view returns (uint256) {
-        return _shares[account];
-    }
-    
-    /**
-     * @dev Returns the address of payee by index
-     */
-    function payee(uint256 index) public view returns (address) {
-        return _payees[index];
-    }
-
-    /**
-     * @dev Returns number of payees
-     */
-    function numPayees() public view returns(uint) {
-        return _payees.length;
-    }
-
-    /**
-     * @dev Returns the total amount of Eth already released
-     */
-    function totalEthReleased() public view returns (uint256) {
-        return _totalEthReleased;
-    }
-
-    /**
-     * @dev Returns the total amount of Tokens already released
-     */
-    function totalTokenReleased() public view returns (uint256) {
-        return _totalTokenReleased;
-    }
-
-    /**
-     * @dev Returns the amount of Eth released to an address
-     */
-    function ethReleased(address account) public view returns (uint256) {
-        return _ethReleased[account];
-    }
-
-    /**
-     * @dev Returns the amount of tokens released to an address
-     */
-    function tokenReleased(address account) public view returns (uint256) {
-        return _tokenReleased[account];
-    }
-
-    /**
-     * @dev Returns the eth balance of contract (not necessry but included for simplicity + symmetry)
-     */
-    function ethBalance() public view returns (uint256) {
-        return address(this).balance;
-    }
-
-    /**
-     * @dev Returns the amount of Eth released to an address
-     */
-    function tokenBalance() public view returns (uint256) { 
-        return IERC20(paymentToken).balanceOf(address(this));
-    }
-
-    /**
-     * @dev Releases funds to account nu address
-     */
-    function release(address payable account) public virtual {
-        
-        require(_payees.length > 0, "PaymentSplitterCloneable: not initialized");
-        require(_shares[account] > 0, "PaymentSplitter: account has no shares");
-
-        uint256 tokenTotalReceived = IERC20(paymentToken).balanceOf(address(this)) + _totalTokenReleased;
-        uint256 tokensOwed = (tokenTotalReceived * _shares[account]) / _totalShares - _tokenReleased[account];
-
-        uint256 totalEthReceived = address(this).balance + _totalEthReleased;
-        uint256 ethOwed = (totalEthReceived * _shares[account]) / _totalShares - _ethReleased[account];
-
-        if (tokensOwed > 0) { // Release tokens to user
-            _tokenReleased[account] = _tokenReleased[account] + tokensOwed;
-            _totalTokenReleased = _totalTokenReleased + tokensOwed;
-
-            IERC20(paymentToken).safeTransfer(account, tokensOwed);
-            emit PaymentReleased(account, tokensOwed);
-        }
-        if (ethOwed > 0) { // Release ERC20 to user
-            _ethReleased[account] = _ethReleased[account] + ethOwed;
-            _totalEthReleased = _totalEthReleased + ethOwed;
-
-            Address.sendValue(account, ethOwed);
-            emit PaymentReleased(account, ethOwed);
-        }
-    }
-
-    /**
-     * @dev Appends payee to list of payees
-     */
-    function _addPayee(address account, uint256 shares_) private {
-        require(account != address(0), "PaymentSplitter: account is the zero address");
-        require(shares_ > 0, "PaymentSplitter: shares are 0");
-        require(_shares[account] == 0, "PaymentSplitter: account already has shares");
-
-        _payees.push(account);
-        _shares[account] = shares_;
-        _totalShares = _totalShares + shares_;
-        emit PayeeAdded(account, shares_);
-    }
 }
--- src/PaymentSplitterManagerClones.sol
+++ src/PaymentSplitterManagerClones.sol
@@ -1,31 +1,34 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/proxy/Clones.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/proxy/Clones.sol";
 import "./PaymentSplitterCloneable.sol";
 
-// #TODO migrate to import payment splitter from oz
-
-/**
- * @title PaymentSplitterManagerClones
- * @dev This contract allows users to create and track minimal proxy contracts (clones) of an implementation of PaymentSplitter
- *
- */
 contract PaymentSplitterManagerClones is Ownable {
+        
+    ////////////////
+    // STATES
+    ////////////////
+
+    uint256 public tax = 1;
+
     mapping(address => address[]) private _paymentTokens;
     mapping(address => address[]) private _createdSplitters;
     mapping(address => address[]) private _registeredSplitters;
+
     address[] public splitters;
+        
+    ////////////////
+    // EVENTS
+    ////////////////
 
-    uint256 public tax = 1;
-    
     event PaymentSplitterCreated(address newSplitter);
+    
+    ////////////////
+    // CONSTRUCTOR
+    ////////////////
 
-    /**
-     * @dev Creates an instance of `PaymentSplitterManagerClones`.
-     */
     constructor () {
         PaymentSplitterCloneable implementation = new PaymentSplitterCloneable();
         address[] memory payees_ = new address[](1);
@@ -39,53 +42,75 @@ contract PaymentSplitterManagerClones is Ownable {
         _registeredSplitters[address(this)].push(address(implementation));
     }
 
-    /**
-     * @dev Getter for the address of the PaymentSplitterCloneable implementation contract.
-     */
-    function splitterImplementation() public view returns (address) {
+    //////////////////////////////////////////////
+    // GETTERS
+    //////////////////////////////////////////////
+    
+    // @notice Getter for the address of the PaymentSplitterCloneable implementation contract.
+    function splitterImplementation()
+        public
+        view
+        returns (address)
+    {
         return splitters[0];
     }
 
-    /**
-     * @dev Getter for the number of PaymentSplitters registered where `_target` has shares.
-     */
-    function registeredCountOf(address _target) external view returns (uint) {
+    // @notice Getter for the number of PaymentSplitters registered where `_target` has shares.
+    function registeredCountOf(
+        address _target
+    )
+        external
+        view
+        returns (uint)
+    {
         return _registeredSplitters[_target].length;
     }
 
-    /**
-     * @dev Getter for the addresses of the PaymentSplitters registered where `_target` has shares.
-     */
-    function registeredSplittersOf(address _target) external view returns (address[] memory) {
+    // @notice Getter for the addresses of the PaymentSplitters registered where `_target` has shares.
+    function registeredSplittersOf(
+        address _target
+    )
+        external
+        view
+        returns (address[] memory)
+    {
         return _registeredSplitters[_target];
     }
 
-    /**
-     * @dev Getter for the address of the PaymentSplitters created by `_target`.
-     */
-    function createdSplittersOf(address _target) external view returns (address[] memory) {
+    // @notice Getter for the address of the PaymentSplitters created by `_target`.
+    function createdSplittersOf(
+        address _target
+    )
+        external
+        view
+        returns (address[] memory)
+    {
         return _createdSplitters[_target];
     }
 
-    /**
-     * @dev Set the price of clones.
-     */
-    function setTax(uint _tax) external onlyOwner {
+    // @notice Set the price of clones.
+    function setTax(
+        uint _tax
+    )
+        external
+        onlyOwner
+    {
         tax = _tax;
     }
 
-    /**
-     * @dev Getter for the payment token of splitter instance
-     */
-    function paymentTokenOf(address _target) external view returns (address[] memory) {
-        return _paymentTokens[_target];
-    }
+    //////////////////////////////////////////////
+    // SPLITTER FUNTIONS
+    //////////////////////////////////////////////
 
-    /**
-     * @dev Spawn a new PaymentSplitter passing in `payees_` and `shares_` to its initializer, and 
-     * records the splitter in memory.
-     */
-    function newSplitter(address[] memory payees_, uint256[] memory shares_, address paymentToken_) external payable {
+    // @notice Spawn a new PaymentSplitter passing in `payees_` and `shares_` to its initializer, and records the splitter in memory.
+    function newSplitter(
+        address[] memory payees_, 
+        uint256[] memory shares_,
+        address paymentToken_
+    )
+        external
+        payable
+    {
         require(msg.value * 1e18 >= tax);
         address _newSplitter = Clones.clone(splitterImplementation());
         PaymentSplitterCloneable(payable(_newSplitter)).initialize(payees_, shares_, paymentToken_);
@@ -97,62 +122,83 @@ contract PaymentSplitterManagerClones is Ownable {
         }
     }
 
-    /**
-     * @dev Release funds associated with the address `_recv`. `_ids` is an array of indexes into 
-     * `_registeredSplitters`.
-     */
-    function shakeIndex(address payable _recv, uint [] memory _ids) external {
+    // @notice Release funds associated with the address `_receiver`. `_ids` is an array of indexes into `_registeredSplitters`.
+    function shakeIndex(
+        address payable _receiver, 
+        uint [] memory _ids
+    )
+        external
+    {
         for(uint i = 0; i < _ids.length; i++) {
-            PaymentSplitterCloneable(payable(_registeredSplitters[_recv][_ids[i]])).release(_recv);
+            PaymentSplitterCloneable(payable(_registeredSplitters[_receiver][_ids[i]])).release(_receiver);
         }
     }
 
-    /**
-     * @dev Release funds associated with the address `_recv`. `_start` and `_end` are bounds on
-     * indexes in `_registeredSplitters`.
-     */
-    function shakeRange(address payable _recv, uint _start, uint _end) external {
+    // @notice Release funds associated with the address `_receiver`. `_start` and `_end` are bounds on indexes in `_registeredSplitters`.
+    function shakeRange(
+        address payable _receiver,
+        uint _start,
+        uint _end
+    )
+        external
+    {
         for(uint i = _start; i < _end; i++) {
-            PaymentSplitterCloneable(payable(_registeredSplitters[_recv][i])).release(_recv);
+            PaymentSplitterCloneable(payable(_registeredSplitters[_receiver][i])).release(_receiver);
         }
     }
 
-    /**
-     * @dev Release all funds associated with the address `_recv`.
-     */
-    function shakeAll(address payable _recv) external {
-        for(uint i = 0; i < _registeredSplitters[_recv].length; i++){
-            PaymentSplitterCloneable(payable(_registeredSplitters[_recv][i])).release(_recv);
+    // @notice Release all funds associated with the address `_receiver`.
+    function shakeAll(
+        address payable _receiver
+    )
+        external
+    {
+        for(uint i = 0; i < _registeredSplitters[_receiver].length; i++){
+            PaymentSplitterCloneable(payable(_registeredSplitters[_receiver][i])).release(_receiver);
         }
     }
 
-    /**
-     * @dev Admin function to collect tax.
-     */
-    function release(address payable _recv, uint _amount) external onlyOwner {
-        Address.sendValue(_recv, _amount);
+    // @notice Admin function to collect tax.
+    function release(
+        address payable _receiver,
+        uint _amount
+    )
+        external
+        onlyOwner
+    {
+        Address.sendValue(_receiver, _amount);
     }
 
-    
-    /**
-     * @dev Admin function to collect all tax.
-     */
-    function withdrawAll(address payable withdrawTo) external onlyOwner {
+    // @notice Admin function to collect all tax.
+    function withdrawAll(
+        address payable withdrawTo
+    )
+        external
+        onlyOwner
+    {
         withdrawTo.transfer(address(this).balance);
     }
 
-    // Views
-    /**
-     * @dev Getter helper for the amount of shares held by an account.
-     */
-    function sharesOfAccount(address splitter, address account) public view returns (uint256) {
+    // @notice Getter helper for the amount of shares held by an account.
+    function sharesOfAccount(
+        address splitter,
+        address account
+    )
+        public
+        view
+        returns (uint256)
+    {
         return PaymentSplitterCloneable(payable(splitter)).shares(account);
     }
 
-    /**
-    * @dev Getter helper for the shares distribution of the splitter at `splitter`.
-    */
-    function shares(address splitter) public view returns (uint256[] memory) {
+    // @notice Getter helper for the shares distribution of the splitter at `splitter`.
+    function shares(
+        address splitter
+    )
+        public
+        view
+        returns (uint256[] memory)
+    {
         PaymentSplitterCloneable psc = PaymentSplitterCloneable(payable(splitter));
 
         uint numPayees = psc.numPayees();
@@ -164,17 +210,26 @@ contract PaymentSplitterManagerClones is Ownable {
         return shares_;
     }
 
-    /**
-    * @dev Getter helper for the payee number `index` of the splitter `splitter`.
-    */
-    function payee(address splitter, uint256 index) public view returns (address) {
+    // @notice Getter helper for the payee number `index` of the splitter `splitter`.
+    function payee(
+        address splitter,
+        uint256 index
+    )
+        public
+        view
+        returns (address)
+    {
         return PaymentSplitterCloneable(payable(splitter)).payee(index);
     }
 
-    /**
-    * @dev Getter helper for the payees of the splitter at `splitter`.
-    */
-    function payees(address splitter) public view returns (address[] memory) {
+    // @notice Getter helper for the payees of the splitter at `splitter`.
+    function payees(
+        address splitter
+    )
+        public
+        view
+        returns (address[] memory)
+    {
         PaymentSplitterCloneable psc = PaymentSplitterCloneable(payable(splitter));
         uint numPayees = psc.numPayees();
         address[] memory payees_ = new address[](numPayees);
@@ -184,95 +239,34 @@ contract PaymentSplitterManagerClones is Ownable {
         return payees_;
     }
 
-    /**
-    * @dev Getter helper for the current releaseable funds associated with a specific payee at
-    * at `splitter`.
-    */
-
-    function balanceOf(address splitter, address payeeAddress) public view returns (uint256, uint256) {
-        PaymentSplitterCloneable psc = PaymentSplitterCloneable(payable(splitter));
-
-        uint256 ethBalance = psc.ethBalance();
-        uint256 tokenBalance = psc.tokenBalance();
-
-        uint256 ethBalance_ = uint256(psc.numPayees());
-        uint256 tokenBalance_ = uint256(psc.numPayees());
-
-        uint256 totalEthReceived = ethBalance + psc.totalEthReleased();
-        uint256 totalTokenReceived = tokenBalance + psc.totalTokenReleased();
-
-        uint256 shares_ = psc.shares(payeeAddress);
-        
-        uint256 ethReleased = psc.ethReleased(payeeAddress);
-        ethBalance_ = (totalEthReceived * shares_) / psc.totalShares() - ethReleased;
-
-        uint256 tokenReleased = psc.tokenReleased(payeeAddress);
-        tokenBalance_ = (totalTokenReceived * shares_) / psc.totalShares() - tokenReleased;
-
-        return (ethBalance_, tokenBalance_);
+    // @notice Getter helper for the current releaseable funds associated with a specific payee at at `splitter`.
+    function balanceOf(
+        address splitter,
+        address account
+    )
+        public
+        view
+        returns (uint256)
+    {
+        return PaymentSplitterCloneable(payable(splitter)).balanceOf(account);
     }
 
-
-
-    /**
-    * @dev Getter helper for the current releaseable funds associated with each payee in the 
-    * splitter at `splitter`.
-    */
-    function balances(address splitter) public view returns (uint256[] memory) {
+    // @notice Getter helper for the current releaseable funds associated with each payee in the  splitter at `splitter`.
+    function balances(
+        address splitter
+    )
+        public
+        view
+        returns (uint256[] memory)
+    {
         PaymentSplitterCloneable psc = PaymentSplitterCloneable(payable(splitter));
-        uint256 totalShares = psc.totalShares();
         uint numPayees = psc.numPayees();
-
-        uint256 ethBalance = psc.ethBalance();
-        uint256 totalEthReleased = psc.totalEthReleased();
-        uint256[] memory ethBalances_ = new uint256[](numPayees);
-        uint256 totalEthReceived = ethBalance + totalEthReleased;
+        uint256[] memory balances_ = new uint256[](numPayees);
         for (uint i = 0; i < numPayees; i++) {
-            address payeeAddress = psc.payee(i);
-            uint256 shares_ = psc.shares(payeeAddress);
-            uint256 ethReleased = psc.ethReleased(payeeAddress);
-            ethBalances_[i] = (totalEthReceived * shares_) / totalShares - ethReleased;
+            address p = psc.payee(i); 
+            balances_[i] = psc.balanceOf(p);
         }
-
-        return ethBalances_;
+        return balances_;
     }
-
-
-    
-    // /**
-    // * @dev Getter helper for the current releaseable funds associated with each payee in the 
-    // * splitter at `splitter`.
-    // */
-    // function balances(address splitter) public view returns (uint256[] memory, uint256[] memory) {
-    //     PaymentSplitterCloneable psc = PaymentSplitterCloneable(payable(splitter));
-
-    //     uint256 totalShares = psc.totalShares();
-    //     uint numPayees = psc.numPayees();
-
-    //     uint256 ethBalance = psc.ethBalance();
-    //     uint256 tokenBalance = psc.tokenBalance();
-
-    //     uint256 totalEthReleased = psc.totalEthReleased();
-    //     uint256 totalTokenReleased = psc.totalTokenReleased();
-
-    //     uint256[] memory ethBalances_ = new uint256[](numPayees);
-    //     uint256[] memory tokenBalances_ = new uint256[](numPayees);
-
-    //     uint256 totalEthReceived = ethBalance + totalEthReleased;
-    //     uint256 totalTokenReceived = tokenBalance + totalTokenReleased;
-
-    //     for (uint i = 0; i < numPayees; i++) {
-    //         address payeeAddress = psc.payee(i);
-    //         uint256 shares_ = psc.shares(payeeAddress);
-            
-    //         uint256 ethReleased = psc.ethReleased(payeeAddress);
-    //         ethBalances_[i] = (totalEthReceived * shares_) / totalShares - ethReleased;
-
-    //         uint256 tokenReleased = psc.tokenReleased(payeeAddress);
-    //         tokenBalances_[i] = (totalTokenReceived * shares_) / totalShares - tokenReleased;
-    //     }
-
-    //     return (ethBalances_, tokenBalances_);
-    // }
 
 }
